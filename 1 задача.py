166a2bb557977b1f8d51e739bf38415d21afe5fe
akimbai <78410543+akimbai@users.noreply.github.com>
2021-02-16 13:18:29 +0600

    ЗАдачи на линейный алгоритм

---
+++ 1 задача.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+A=0
+B=0 
+C=A
+A=B
+B=C
+print('A',A)
+print('B',B)
+
+
+# In[13]:
+
+
+A=0
+B=5 
+C=A
+A=B
+B=C
+print('A',A)
+print('B',B)
+
+
+# In[14]:
+
+
+A=10
+B=20 
+C=A
+A=B
+B=C
+print('A',A)
+print('B',B)
+
+
+# In[15]:
+
+
+A=10
+B=10 
+C=A
+A=B
+B=C
+print('A',A)
+print('B',B)
+
+
+# In[ ]:
+
+
+
+
