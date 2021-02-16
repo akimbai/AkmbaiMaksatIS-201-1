d52bc9f5b6da703dd5469c1e83d78c2127a88316
akimbai <78410543+akimbai@users.noreply.github.com>
2021-02-16 12:54:34 +0600

    1!

---
+++ Untitled.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=0 
+while a<7:
+    print("A")
+    a+=1
+
+
+# In[11]:
+
+
+a=6
+while a>=0:
+    if a==7:
+        break
+    a+=1
+    print("A")
+    
+
+
+# In[12]:
+
+
+a=-1
+while a<10:
+    a+=1
+    if a>=7:
+        continue
+    print("A")
+
+
+# In[16]:
+
+
+lst =[1994,1333,5145,4,6]
+for a in lst:
+    print(a**2)
+
+
+# In[ ]:
+
+
+
+
