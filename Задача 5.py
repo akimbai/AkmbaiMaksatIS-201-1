0b8524ad6ff8a3012d8a76e670cce2783ea2ea4b
akimbai <78410543+akimbai@users.noreply.github.com>
2021-02-16 13:32:47 +0600

    Задачи на разветвляющийся алгоритм

---
+++ Задача 5.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[13]:
+
+
+import math
+x=input('x=')
+x=int(x)
+if x>=0:
+    y=math.sqrt(x)
+elif x>=0:
+        y=x**2
+print('x=',x)
+print('y=',y)
+
+
+# In[14]:
+
+
+import math
+x=input('x=')
+x=int(x)
+if x>=0:
+    y=math.sqrt(x)
+elif x>=0:
+        y=x**2
+print('x=',x)
+print('y=',y)
+
+
+# In[15]:
+
+
+import math
+x=input('x=')
+x=int(x)
+if x>=0:
+    y=math.sqrt(x)
+elif x>=0:
+        y=x**2
+print('x=',x)
+print('y=',y)
+
+
+# In[ ]:
+
+
+
+
