73610291ad0da15b08d1992c476e95c34485a7f9
akimbai <78410543+akimbai@users.noreply.github.com>
2021-02-17 17:11:44 +0600

    Add files via upload

---
+++ 7 задача.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+import math
+x=input('x=')
+x=int(x)
+y=input('y=')
+y=int(y)
+if x<=0:
+    print("Ошибка")
+elif x>=0:
+    z=y/x
+    print('x=',x)
+    print('y=',y)
+    print('z=',z)
+
+
+
+# In[9]:
+
+
+import math
+x=input('x=')
+x=int(x)
+y=input('y=')
+y=int(y)
+if x<=0:
+    print("Ошибка")
+elif x>=0:
+    z=y/x
+    print('x=',x)
+    print('y=',y)
+    print('z=',z)
+
+
+# In[10]:
+
+
+import math
+x=input('x=')
+x=int(x)
+y=input('y=')
+y=int(y)
+if x<=0:
+    print("Ошибка")
+elif x>=0:
+    z=y/x
+    print('x=',x)
+    print('y=',y)
+    print('z=',z)
+
+
+# In[ ]:
+
+
+
+
