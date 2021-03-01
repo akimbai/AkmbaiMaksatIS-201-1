cad78995552812297b6c8c25bd1ba55be1af8df5
akimbai <78410543+akimbai@users.noreply.github.com>
2021-03-01 18:32:52 +0600

    Дополнительные задачи

---
+++ 14 задача.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+import math 
+x=int(input("x="))
+if x<0:
+    y=x**2+1
+    print('y=',y)
+else:
+    if x>=1:
+        y=4*x-1
+        print('y=',y)
+    else:
+        y=2*x+1
+        print('y=',y)
+    
+
+
+# In[6]:
+
+
+import math 
+x=int(input("x="))
+if x<0:
+    y=x**2+1
+    print('y=',y)
+else:
+    if x>=1:
+        y=4*x-1
+        print('y=',y)
+    else:
+        y=2*x+1
+        print('y=',y)
+    
+
+
+# In[ ]:
+
+
+
+
---
+++ 15 задача.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+import math 
+a=int(input("a="))
+if a>0:
+    print('x=любое')
+else:
+    c=3/a
+    if a>0:
+        x<c
+        print('x=',x)
+    else:
+        x>c
+        print('x=',x)
+    
+
+
+# In[ ]:
+
+
+
+
